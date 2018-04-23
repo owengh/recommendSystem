51e904a3dde1b52062b0ea9ec02df9ab1cff9aa2
wangwenjing <wangwenjing@A201709011811>
2018-04-23 20:07:33 +0800

    commit

--- src/main/java/com/cognition/bl/Application.java
+++ src/main/java/com/cognition/bl/Application.java
@@ -1,10 +1,12 @@
 package com.cognition.bl;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.cognition.bl")
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/cognition/bl/controller/HelloWorld.java
+++ src/main/java/com/cognition/bl/controller/HelloWorld.java
@@ -1,19 +1,28 @@
 package com.cognition.bl.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.cognition.bl.domain.SalesOrder;
+
 @RestController
 public class HelloWorld {
+	
+//	@Autowired
+//	EmpRepository empRepository;
 
 	@RequestMapping("/hello")
 	public String hello(){
-		
+//		System.out.println(empRepository);
+//		List<SalesOrder> list = empRepository.queryName("test01");
 		return "hello world";
 	}
 	
